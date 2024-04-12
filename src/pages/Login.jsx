731b5bde2297a37b5930dc86f413a3dd5a15bde0
mansijaiswal <96405456+mansijaiswal12@users.noreply.github.com>
2024-04-12 16:12:54 +0530

    Login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react'
+import { useFirebase } from "../context/Firebase"
+import { useNavigate } from 'react-router-dom'
+
+
+const Login = () => {
+
+  const firebase = useFirebase()
+  const [email, setemail] = useState('')
+  const [password, setpassword] = useState('')
+  const navigate = useNavigate()
+
+  const handlesubmit = async (e) => {
+    e.preventDefault()
+    const result = await firebase.signinWithEmailAndPassword(email, password)
+    console.log(result)
+  }
+
+    const linkSignup = () =>{
+           navigate('/signUp')
+    }
+
+  return (
+    <>
+
+
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form onSubmit={handlesubmit} className="space-y-6" >
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input onChange={e => setemail(e.target.value)} value={email}
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input onChange={e => setpassword(e.target.value)} value={password}
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+
+          <button
+            type="click" onClick={firebase.signinWithGoogle}
+            className="flex w-75 justify-center rounded-md bg-red-600 px-3 ml-[7rem] mt-5 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Login with Google
+          </button>
+
+          <button
+            type="click" onClick={linkSignup}
+            className="flex w-75 justify-center rounded-md bg-red-600 px-3 ml-[7rem] mt-5 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Signup
+          </button>
+
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Login
+
